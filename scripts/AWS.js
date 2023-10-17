0e9e7269cee468b19c8f01c565e8850ea041cd8d
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-17 12:07:56 +0200

    aws script working with env

---
+++ scripts/AWS.js
@@ -0,0 +1,33 @@
+require('dotenv').config({ path: '../.env' }); 
+process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY;
+process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
+
+const { exec } = require('child_process');
+
+async function fetchAWSSpotPrices() {
+    return new Promise((resolve, reject) => {
+        exec('aws ec2 describe-spot-price-history --region us-east-1 --max-items 5', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error: ${error.message}`);
+                return reject(error);
+            }
+            if (stderr) {
+                console.error(`Stderr: ${stderr}`);
+                return reject(new Error(stderr));
+            }
+
+            // Parse the CLI output to JSON
+            const result = JSON.parse(stdout);
+            return resolve(result);
+        });
+    });
+}
+
+// Call the function to start fetching data
+fetchAWSSpotPrices()
+    .then(result => {
+        console.log('AWS Spot Prices:', result);
+    })
+    .catch(error => {
+        console.error('Error fetching AWS Spot Prices:', error.message);
+    });
