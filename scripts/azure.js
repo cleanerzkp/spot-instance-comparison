8a699ef2545c11b1935101723826677eeae03d2a
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-21 19:10:38 +0200

    ready for new database design

--- scripts/azure.js
+++ scripts/azure.js
@@ -1,55 +1,27 @@
-'use strict';
-
 const axios = require('axios');
-const db = require('../models/index');
-const SpotInstancePricing = db.SpotInstancePricing;
+const moment = require('moment'); 
+
 async function fetchAzureSpotPrices() {
     try {
         const params = {
-            "$filter": "serviceName eq 'Virtual Machines' and contains(meterName, 'Spot')",
-            "$top": 10000, // Limit the result to 100 rows
+            "$filter": "serviceName eq 'Virtual Machines' and armSkuName eq 'Standard_NP20s' and armRegionName eq 'southcentralus' and contains(meterName, 'Spot')",
+            "$top": 10000,
             "api-version": "2023-01-01-preview"
         };
 
         const response = await axios.get('https://prices.azure.com/api/retail/prices', { params });
 
         if (response.status === 200) {
-            for (const item of response.data.Items) {
-                // Check if a record with the same properties already exists
-                const existingRecord = await SpotInstancePricing.findOne({
-                    where: {
-                        CloudProvider: 'Azure',
-                        InstanceType: item.armSkuName,
-                        Region: item.armRegionName,
-                        PricePerHour_USD: item.retailPrice,
-                        EffectiveStartDate: item.effectiveStartDate,
-                    }
-                });
-
-                if (!existingRecord) {
-                    // If no existing record is found, create a new one
-                    await SpotInstancePricing.create({
-                        CloudProvider: 'Azure',
-                        InstanceType: item.armSkuName,
-                        Region: item.armRegionName,
-                        PricePerHour_USD: item.retailPrice,
-                        EffectiveStartDate: item.effectiveStartDate,
-                        OriginalAPIResponse: JSON.stringify(item),
-                        Location: item.location,
-                        MeterName: item.meterName,
-                        ProductName: item.productName,
-                        SkuName: item.skuName,
-                        ServiceName: item.serviceName,
-                        ServiceFamily: item.serviceFamily,
-                        UnitOfMeasure: item.unitOfMeasure,
-                    });
-                    console.log('New data inserted successfully.');
-                } else {
-                    // If an existing record is found, you can update it if needed or log a message.
-                    console.log('Duplicate data found and skipped.');
-                }
-            }
-            console.log('Data insertion completed.');
+            const standardizedData = response.data.Items.map(item => {
+                return {
+                    SpotPrice: item.retailPrice,
+                    Timestamp: item.effectiveStartDate,
+                    AvailabilityZone: item.armRegionName,
+                    InstanceType: item.armSkuName,
+                    ProductDescription: item.serviceName
+                };
+            });
+            console.log('Standardized Data:', standardizedData);
         } else {
             console.error(`Request failed with status ${response.status}`);
         }
@@ -58,5 +30,4 @@ async function fetchAzureSpotPrices() {
     }
 }
 
-// Call the function to start inserting data
 fetchAzureSpotPrices();
