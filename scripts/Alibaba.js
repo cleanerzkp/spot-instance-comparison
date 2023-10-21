67774aff081c82178a5f9cdc3adc9cacb820526d
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-22 00:49:17 +0200

    alibaba still not it

--- scripts/Alibaba.js
+++ scripts/Alibaba.js
@@ -4,7 +4,7 @@ const ALIBABA_ACCESS_KEY_SECRET = process.env.ALIBABA_ACCESS_KEY_SECRET;
 
 const { RPCClient } = require('@alicloud/pop-core');
 
-const regions = ['us-west-1', 'eu-central-1'];
+const regions = ['us-west-1', 'asia-southeast1'];
 const instanceTypes = ['ecs.g6a.xlarge', 'ecs.g6.xlarge'];  
 async function fetchAlibabaSpotPrices() {
     const client = new RPCClient({
