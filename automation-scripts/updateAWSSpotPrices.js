8d687d6480ba55295dbcb61ee1856076adc28ca1
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-29 13:21:04 +0100

    updated AWS cron

--- automation-scripts/updateAWSSpotPrices.js
+++ automation-scripts/updateAWSSpotPrices.js
@@ -1,7 +1,9 @@
 const db = require('../testDbConnection');
 const SpotPricing = db.SpotPricing;
 const { exec } = require('child_process');
+const cron = require('node-cron');
 
+// Function to fetch AWS spot prices
 async function fetchAWSSpotPrices(instanceType, region) {
   return new Promise((resolve, reject) => {
     exec(`aws ec2 describe-spot-price-history --instance-types ${instanceType} --region ${region} --max-items 1000 --product-descriptions "Linux/UNIX" --output json`, (error, stdout, stderr) => {
@@ -19,30 +21,56 @@ async function fetchAWSSpotPrices(instanceType, region) {
   });
 }
 
+// Function to insert data into the database
 async function insertIntoDB(dailyAverages, instanceType, region) {
   const groupingMap = {
-    't4g.xlarge': 'GO1',
+    't4g.xlarge': 'GP1',
     'c6a.xlarge': 'CO1'
   };
 
   for (const date in dailyAverages) {
-    const price = dailyAverages[date];
-    await SpotPricing.create({
-      name: `${instanceType}-general-purpose`,
-      regionCategory: `AWS-${region}`,
-      date: new Date(date),
-      price: price,
-      timestamp: new Date(),
-      grouping: groupingMap[instanceType],
-      providerID: 'AWS'
+    const existingRecord = await SpotPricing.findOne({
+      where: {
+        name: `${instanceType}-general-purpose`,
+        date: new Date(date),
+        regionCategory: `AWS-${region}`
+      }
     });
+
+    const today = new Date().toISOString().split('T')[0];
+    if (existingRecord && date !== today) {
+      continue;
+    }
+
+    const price = dailyAverages[date];
+
+    if (existingRecord && date === today) {
+      await existingRecord.update({ price: price });
+    } else {
+      await SpotPricing.create({
+        name: `${instanceType}-general-purpose`,
+        regionCategory: `AWS-${region}`,
+        date: new Date(date),
+        price: price,
+        timestamp: new Date(),
+        grouping: groupingMap[instanceType],
+        providerID: 'AWS'
+      });
+    }
   }
 }
 
+// Function to calculate daily average of spot prices
 async function calculateDailyAverage(instanceType, region) {
   try {
     const result = await fetchAWSSpotPrices(instanceType, region);
     const spotPriceHistory = result.SpotPriceHistory;
+    
+    if (spotPriceHistory.length === 0) {
+      console.log(`No prices available for ${instanceType} in ${region}`);
+      return;
+    }
+
     const pricesByDay = {};
     const dailyAverages = {};
 
@@ -68,11 +96,14 @@ async function calculateDailyAverage(instanceType, region) {
   }
 }
 
-const instances = ['t4g.xlarge', 'c6a.xlarge'];
-const regions = ['us-east-1', 'eu-west-1'];
+// Scheduled task to run every day at 12:00 AM
+cron.schedule('0 0 * * *', function() {
+  const instances = ['t4g.xlarge', 'c6a.xlarge'];
+  const regions = ['us-east-1', 'eu-west-1'];
 
-instances.forEach(instanceType => {
-  regions.forEach(region => {
-    calculateDailyAverage(instanceType, region);
+  instances.forEach(instanceType => {
+    regions.forEach(region => {
+      calculateDailyAverage(instanceType, region);
+    });
   });
 });
