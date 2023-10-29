6bf9e8132b50ce3474e1cd5cc9453c42468f36d0
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-29 21:46:42 +0100

    still not working

--- historical-data/fetchAWSHistoricalPrices.js
+++ historical-data/fetchAWSHistoricalPrices.js
@@ -2,7 +2,6 @@ require('dotenv').config({ path: '../.env' });
 process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY;
 process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
 
-
 const { exec } = require('child_process');
 const db = require('../models');
 const SpotPricing = db.SpotPricing;
@@ -11,7 +10,7 @@ const Region = db.Region;
 
 async function fetchAWSSpotPrices(instanceType, region) {
   return new Promise((resolve, reject) => {
-    exec(`aws ec2 describe-spot-price-history --instance-types ${instanceType} --region ${region} --max-items 1000 --product-descriptions "Linux/UNIX" --output json`, (error, stdout, stderr) => {
+    exec(`aws ec2 describe-spot-price-history --instance-types ${instanceType} --region ${region} --max-items 10 --product-descriptions "Linux/UNIX" --output json`, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error for ${instanceType} in ${region}: ${error.message}`);
         return reject(error);
@@ -20,40 +19,51 @@ async function fetchAWSSpotPrices(instanceType, region) {
         console.error(`Stderr for ${instanceType} in ${region}: ${stderr}`);
         return reject(new Error(stderr));
       }
-      const result = JSON.parse(stdout);
-      return resolve(result);
+      try {
+        const result = JSON.parse(stdout);
+        return resolve(result);
+      } catch (parseError) {
+        console.error(`JSON parse error for ${instanceType} in ${region}: ${parseError.message}`);
+        return reject(parseError);
+      }
     });
   });
 }
 
 async function saveOrUpdateSpotPrice(region, instanceType, date, price, grouping, providerID) {
-  const dataToInsertOrUpdate = {
-    name: `${instanceType}-general-purpose`,
-    regionCategory: `AWS-${region}`,
-    date,
-    price,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    grouping,
-    providerID
-  };
+  try {
+    const existingRecord = await SpotPricing.findOne({
+      where: {
+        region,
+        instance_type: instanceType,
+        date
+      }
+    });
   
-  const existingRecord = await SpotPricing.findOne({
-    where: {
-      region,
-      instance_type: instanceType,
-      date
+    const dataToInsertOrUpdate = {
+      name: `${instanceType}-${grouping}`,
+      regionCategory: `AWS-${region}`,
+      date,
+      price,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      grouping,
+      providerID
+    };
+  
+    if (existingRecord) {
+      await existingRecord.update(dataToInsertOrUpdate);
+    } else {
+      await SpotPricing.create(dataToInsertOrUpdate);
     }
-  });
-  
-  if (existingRecord) {
-    await existingRecord.update(dataToInsertOrUpdate);
-  } else {
-    await SpotPricing.create(dataToInsertOrUpdate);
+  } catch (dbError) {
+    console.error(`Database error for ${instanceType} in ${region}: ${dbError.message}`);
   }
 }
 
-async function fetchAndSaveAWSSpotPrices(region, instanceType, grouping, providerID) {
+async function fetchAndSaveAWSSpotPrices(region, instanceTypeObj) {
+  const { name: instanceType, grouping, providerID } = instanceTypeObj;
+
   try {
     const result = await fetchAWSSpotPrices(instanceType, region);
     const spotPriceHistory = result.SpotPriceHistory;
@@ -78,10 +88,10 @@ async function fetchAndSaveAWSSpotPrices(region, instanceType, grouping, provide
       const average = sum / prices.length;
       await saveOrUpdateSpotPrice(region, instanceType, date, average, grouping, providerID);
     }
-    
+
     console.log(`Price history is available for ${instanceType} in ${region}.`);
   } catch (error) {
-    // Do nothing, this region-instance pair doesn't have history
+    console.error(`Fetch and save error for ${instanceType} in ${region}: ${error.message}`);
   }
 }
 
@@ -92,12 +102,8 @@ async function main() {
     const regions = awsRegions.map(r => r.name);
 
     for (const instanceTypeObj of awsInstanceTypes) {
-      const instanceType = instanceTypeObj.name;
-      const grouping = instanceTypeObj.grouping;
-      const providerID = "AWS"; // Replace with actual providerID if needed
-
       for (const region of regions) {
-        await fetchAndSaveAWSSpotPrices(region, instanceType, grouping, providerID);
+        await fetchAndSaveAWSSpotPrices(region, instanceTypeObj);
       }
     }
   } catch (error) {
