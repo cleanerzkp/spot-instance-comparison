106b22ac01d5ca1a226738c32ddec7fa6ae0ca2d
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-31 16:51:49 +0100

    google finally works

--- automation-scripts/updateAlibabaSpotPrices.js
+++ automation-scripts/updateAlibabaSpotPrices.js
@@ -2,147 +2,81 @@ require('dotenv').config({ path: '../.env' });
 const ALIBABA_ACCESS_KEY_ID = process.env.ALIBABA_ACCESS_KEY_ID;
 const ALIBABA_ACCESS_KEY_SECRET = process.env.ALIBABA_ACCESS_KEY_SECRET;
 
-const { Sequelize } = require('sequelize');
 const { RPCClient } = require('@alicloud/pop-core');
 const db = require('../models');
-const SpotPricing = db.SpotPricing;
-const InstanceType = db.InstanceType;
-const Region = db.Region;
+const { SpotPricing, InstanceType, Region } = db;
 
 const regionEndpoints = {
   'us-west-1': 'https://ecs.us-west-1.aliyuncs.com',
-  'eu-central-1': 'https://ecs.eu-central-1.aliyuncs.com',
-  'us-east-1': 'https://ecs.us-east-1.aliyuncs.com',
-  'ap-south-1': 'https://ecs.ap-south-1.aliyuncs.com',
-  'me-east-1': 'https://ecs.me-east-1.aliyuncs.com',
+  'eu-central-1': 'https://ecs.eu-central-1.aliyuncs.com'
 };
 
-function generateSubRegions(region) {
-    const subRegions = [];
-    const suffixes = ['a', 'b', 'c'];  // Assuming sub-regions are denoted by a, b, c, etc.
-    suffixes.forEach(suffix => {
-        subRegions.push(`${region}${suffix}`);
-    });
-    return subRegions;
-}
-
-
 async function fetchData() {
-  const instanceTypes = await InstanceType.findAll({ where: { providerID: 'ALB' } });
-  const regions = await Region.findAll({ where: { providerID: 'ALB' } });
+    const instanceTypes = await InstanceType.findAll({ where: { providerID: 'ALB' } });
+    const regions = await Region.findAll({ where: { providerID: 'ALB' } });
+  
+    return {
+      instanceTypes: instanceTypes.map(it => ({ name: it.name, category: it.category, grouping: it.grouping })),
+      regions: regions.map(r => ({ name: r.name, endpoint: regionEndpoints[r.name] || 'missing endpoint' }))
+    };  
+  }
 
-  return {
-    instanceTypes: instanceTypes.map(it => ({ name: it.name, category: it.category, grouping: it.grouping })),
-    regions: regions.map(r => r.name)
-  };  
-}
-
-async function fetchAlibabaSpotPrices(instanceType, subRegion) {
-    // Extract the main region from the sub-region
-    const mainRegion = subRegion.slice(0, -1);
-    const endpoint = regionEndpoints[mainRegion];
-    console.log(`Endpoint for ${subRegion}:`, endpoint);
-
-    if (!endpoint) {
-      console.error(`No endpoint found for region ${subRegion}`);
-      return [];
+  async function fetchAlibabaSpotPrices(instanceType, region) {
+    if (!region.endpoint || !region.endpoint.startsWith('https://')) {
+      console.error(`Invalid or missing endpoint for region ${region.name}`);
+      return;
     }
-
+  
     const client = new RPCClient({
       accessKeyId: ALIBABA_ACCESS_KEY_ID,
       accessKeySecret: ALIBABA_ACCESS_KEY_SECRET,
-      endpoint: endpoint,
+      endpoint: region.endpoint,
       apiVersion: '2014-05-26'
     });
-
+  
+  try {
     const params = {
-      RegionId: subRegion,  // Use subRegion here
+      RegionId: region.name,
       NetworkType: 'vpc',
       InstanceType: instanceType.name,
-      MaxResults: 1000
+      MaxResults: 5,
     };
-
     const result = await client.request('DescribeSpotPriceHistory', params);
     return result.SpotPrices.SpotPriceType;
+  } catch (error) {
+    console.error(`Error fetching Alibaba Spot Prices for ${region.name} ${instanceType.name}:`, error.message);
+  }
 }
 
-
-async function insertIntoDB(dailyAverages, instanceTypeObj, region) {
-    for (const date in dailyAverages) {
-      const existingRecord = await SpotPricing.findOne({
-        where: {
-          name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
-          date: new Date(date),
-          regionCategory: `ALB-${region}`
-        }
-      });
+async function insertIntoDB(data, instanceTypeObj, region) {
+  const mappedData = data.map(spot => ({
+    name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
+        regionCategory: `ALB-${region}`,
+        date: new Date(date),
+        price: price,
+        timestamp: new Date(),
+        grouping: instanceTypeObj.grouping,
+        providerID: 'ALB'
+  }));
   
-      const today = new Date().toISOString().split('T')[0];
-      if (existingRecord && date !== today) {
-        continue;
-      }
-  
-      const price = dailyAverages[date];
-  
-      if (existingRecord && date === today) {
-        await existingRecord.update({ price: price });
-      } else {
-        await SpotPricing.create({
-          name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
-          regionCategory: `ALB-${region}`,
-          date: new Date(date),
-          price: price,
-          timestamp: new Date(),
-          grouping: instanceTypeObj.grouping,
-          providerID: 'ALB'
-        });
-      }
-    }
-  }
-
-async function calculateDailyAverage(instanceTypeObj, region) {
-  try {
-    const spotPriceHistory = await fetchAlibabaSpotPrices(instanceTypeObj, region);
-
-    if (spotPriceHistory.length === 0) {
-      console.log(`No prices available for ${instanceTypeObj.name} in ${region}`);
-      return;
-    }
-
-    const pricesByDay = {};
-    const dailyAverages = {};
-
-    spotPriceHistory.forEach(spotPrice => {
-      const date = new Date(spotPrice.Timestamp).toISOString().split('T')[0];
-      if (!pricesByDay[date]) {
-        pricesByDay[date] = [];
-      }
-      pricesByDay[date].push(parseFloat(spotPrice.SpotPrice));
-    });
-
-    for (const date in pricesByDay) {
-      const prices = pricesByDay[date];
-      const sum = prices.reduce((acc, price) => acc + price, 0);
-      const average = sum / prices.length;
-      dailyAverages[date] = average;
-    }
-
-    await insertIntoDB(dailyAverages, instanceTypeObj, region);
-  } catch (error) {
-    console.error('Error calculating daily average:', error.message);
+  for (const entry of mappedData) {
+    await SpotPricing.create(entry);
   }
 }
 
 async function main() {
-    const { instanceTypes, regions } = await fetchData();
-    instanceTypes.forEach(instanceTypeObj => {
-      regions.forEach(region => {
-          const subRegions = generateSubRegions(region);
-          subRegions.forEach(subRegion => {
-              calculateDailyAverage(instanceTypeObj, subRegion);
-          });
-      });
-    });
+  const { instanceTypes, regions } = await fetchData();
+  
+  for (const instanceTypeObj of instanceTypes) {
+    for (const region of regions) {
+      const data = await fetchAlibabaSpotPrices(instanceTypeObj, region);
+      if (data && data.length > 0) {
+        await insertIntoDB(data, instanceTypeObj, region);
+      }
+    }
   }
   
-  main();
+  console.log('Alibaba data saved successfully');
+}
+
+main();
--- automation-scripts/updateGCPSpotPrices.js
+++ automation-scripts/updateGCPSpotPrices.js
@@ -1,20 +1,57 @@
+const axios = require('axios');
 const { GoogleAuth } = require('google-auth-library');
 const path = require('path');
 
 async function authenticate() {
-  const credentialsPath = path.resolve(__dirname, '..', 'GetSpot-Service-Account.json');
+    const credentialsPath = path.resolve(__dirname, '../GetSpot-Service-Account.json');
 
-  const auth = new GoogleAuth({
-    keyFilename: credentialsPath,
-    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-  });
+    const auth = new GoogleAuth({
+        keyFilename: credentialsPath,
+        scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+    });
 
-  try {
-    const authClient = await auth.getClient();
-    console.log('Authentication successful.');
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+    try {
+        const authClient = await auth.getClient();
+        const accessToken = await authClient.getAccessToken();
+        console.log('Authentication successful. Access Token:', accessToken);
+        return authClient;
+    } catch (error) {
+        console.error('Error during authentication:', error.message);
+        return null;
+    }
 }
 
-authenticate();
+async function getGCPSpotPrices(authClient) {
+    const projectId = 'getspot-402212';  // Your project ID
+    const serviceId = '6F81-5844-456A';  // Replace with your service ID
+
+    const url = `https://cloudbilling.googleapis.com/v1/services/${serviceId}/skus`;
+
+    try {
+        const accessToken = await authClient.getAccessToken();
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken.token}`,
+            },
+        });
+        console.log('Response Data:', response.data);
+    } catch (error) {
+        console.error('Error during API request:', error.message);
+        if (error.response) {
+            console.error('Response data:', error.response.data);
+            console.error('Response status:', error.response.status);
+            console.error('Response headers:', error.response.headers);
+        } else if (error.request) {
+            console.error('Request data:', error.request);
+        }
+    }
+}
+
+async function main() {
+    const authClient = await authenticate();
+    if (authClient) {
+        await getGCPSpotPrices(authClient);
+    }
+}
+
+main();
