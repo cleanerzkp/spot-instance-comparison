0cd2c26151305aed4b87928f9424397e6d92bc6d
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-31 17:14:26 +0100

    working script nice

--- automation-scripts/updateGCPSpotPrices.js
+++ automation-scripts/updateGCPSpotPrices.js
@@ -20,31 +20,51 @@ async function authenticate() {
         return null;
     }
 }
-
 async function getGCPSpotPrices(authClient) {
-    const projectId = 'getspot-402212';  // Your project ID
-    const serviceId = '6F81-5844-456A';  // Replace with your service ID
+  const projectId = 'getspot-402212'; 
+  const serviceId = '6F81-5844-456A';
 
-    const url = `https://cloudbilling.googleapis.com/v1/services/${serviceId}/skus`;
+  const url = `https://cloudbilling.googleapis.com/v1/services/${serviceId}/skus`;
 
-    try {
-        const accessToken = await authClient.getAccessToken();
-        const response = await axios.get(url, {
-            headers: {
-                Authorization: `Bearer ${accessToken.token}`,
-            },
-        });
-        console.log('Response Data:', response.data);
-    } catch (error) {
-        console.error('Error during API request:', error.message);
-        if (error.response) {
-            console.error('Response data:', error.response.data);
-            console.error('Response status:', error.response.status);
-            console.error('Response headers:', error.response.headers);
-        } else if (error.request) {
-            console.error('Request data:', error.request);
-        }
-    }
+  try {
+      const accessToken = await authClient.getAccessToken();
+      const response = await axios.get(url, {
+          headers: {
+              Authorization: `Bearer ${accessToken.token}`,
+          },
+      });
+
+      const items = response.data.skus;
+      const specificSkus = ['4111-7FF1-D50A', '41F4-F6BE-4AF2', /* ...other SKU IDs */];
+      items.forEach(item => {
+          if (specificSkus.includes(item.skuId)) {
+              const description = item.description;
+              const pricingInfo = item.pricingInfo;
+              pricingInfo.forEach(price => {
+                  const pricingExpression = price.pricingExpression;
+                  const baseUnit = pricingExpression.baseUnitDescription;
+                  const tieredRates = pricingExpression.tieredRates;
+                  tieredRates.forEach(rate => {
+                      const unitPrice = rate.unitPrice;
+                      const currencyCode = unitPrice.currencyCode;
+                      const units = unitPrice.units;
+                      const nanos = unitPrice.nanos;
+                      console.log(`${description}: ${currencyCode} ${units}.${nanos}`);
+                  });
+              });
+          }
+      });
+
+  } catch (error) {
+      console.error('Error during API request:', error.message);
+      if (error.response) {
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+          console.error('Response headers:', error.response.headers);
+      } else if (error.request) {
+          console.error('Request data:', error.request);
+      }
+  }
 }
 
 async function main() {
