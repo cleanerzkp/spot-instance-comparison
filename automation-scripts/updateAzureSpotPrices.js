8307eb8c44533b23e244d69e93c175c8e616bba8
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-10-31 15:29:45 +0100

    Azure is working

--- automation-scripts/updateAzureSpotPrices.js
+++ automation-scripts/updateAzureSpotPrices.js
@@ -17,7 +17,7 @@ async function fetchData() {
 async function fetchAzurePrices(instanceType, region, nextPageUrl) {
   const apiUrl = 'https://prices.azure.com/api/retail/prices';
   const params = {
-    '$filter': `armRegionName eq '${region}' and armSkuName eq 'Standard_${instanceType}'`,  // Adjusted the armSkuName value
+    '$filter': `armRegionName eq '${region}' and armSkuName eq 'Standard_${instanceType}'`,
     '$top': 100
   };
 
@@ -44,6 +44,14 @@ async function fetchAzurePrices(instanceType, region, nextPageUrl) {
 async function insertIntoDB(prices, instanceTypeObj, region) {
   for (const price of prices) {
     const date = new Date(price.effectiveStartDate).toISOString().split('T')[0];
+    const priceValue = parseFloat(price.retailPrice);
+    
+    // Ignoring outlier prices
+    if (priceValue > 10) {
+      console.log(`Ignoring outlier price: ${priceValue} for ${instanceTypeObj.name} in ${region}`);
+      continue;
+    }
+
     const existingRecord = await SpotPricing.findOne({
       where: {
         name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
@@ -52,10 +60,11 @@ async function insertIntoDB(prices, instanceTypeObj, region) {
       }
     });
 
-    const priceValue = parseFloat(price.retailPrice);
-
     if (existingRecord) {
-      await existingRecord.update({ price: priceValue });
+      // Updating with new price value if it's different
+      if (existingRecord.price !== priceValue) {
+        await existingRecord.update({ price: priceValue });
+      }
     } else {
       await SpotPricing.create({
         name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
@@ -67,22 +76,31 @@ async function insertIntoDB(prices, instanceTypeObj, region) {
         providerID: 'AZR'
       });
     }
-
-    
   }
 }
+
 async function main() {
-    const { instanceTypes, regions } = await fetchData();
-    for (const instanceTypeObj of instanceTypes) {
-      for (const region of regions) {
+  const { instanceTypes, regions } = await fetchData();
+  const promises = [];
+
+  for (const instanceTypeObj of instanceTypes) {
+    for (const region of regions) {
+      promises.push((async () => {
         const prices = await fetchAzurePrices(instanceTypeObj.name, region);
-        if (prices.length > 0) {
-          await insertIntoDB(prices, instanceTypeObj, region);
+        const filteredPrices = prices.filter(price => {
+          const date = new Date(price.effectiveStartDate);
+          return date >= new Date('2023-10-01T00:00:00Z') && date <= new Date('2023-10-31T23:59:59Z');
+        });
+        if (filteredPrices.length > 0) {
+          await insertIntoDB(filteredPrices, instanceTypeObj, region);
         } else {
           console.log(`No prices available for ${instanceTypeObj.name} in ${region}`);
         }
-      }
+      })());
     }
   }
-  
-  main();
+
+  await Promise.all(promises);
+}
+
+main();
--- automation-scripts/updateGCPSpotPrices.js
+++ automation-scripts/updateGCPSpotPrices.js
@@ -1,59 +1,20 @@
-require('dotenv').config();
-const { google } = require('googleapis');
 const { GoogleAuth } = require('google-auth-library');
 const path = require('path');
-const fs = require('fs');
 
-async function main() {
-  // Load client secrets from a file, and setup the GoogleAuth client
+async function authenticate() {
   const credentialsPath = path.resolve(__dirname, '..', 'GetSpot-Service-Account.json');
-  const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf8'));
+
   const auth = new GoogleAuth({
-    credentials,
+    keyFilename: credentialsPath,
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
   });
 
-  // Obtain client for making requests
-  const authClient = await auth.getClient();
-  const cloudbilling = google.cloudbilling('v1');
-  const request = {
-    auth: authClient,
-    parent: `projects/${credentials.project_id}/services/compute.googleapis.com`,
-    pageSize: 100,
-  };
-
-  let skus = [];
-  let nextPageToken;
-  const targetInstances = ['e2-standard-4', 'c2-standard-4'];
-
-  do {
-    if (nextPageToken) {
-      request.pageToken = nextPageToken;
-    }
-
-    const result = await cloudbilling.services.skus.list(request);
-    skus = skus.concat(result.data.skus || []);
-    nextPageToken = result.data.nextPageToken;
-
-  } while (nextPageToken);
-
-  const spotSkus = skus.filter(sku => {
-    return sku.category && sku.category.usageType === 'Preemptible' &&
-           targetInstances.some(instance => sku.description.includes(instance));
-  });
-
-  // Format the output
-  spotSkus.forEach(sku => {
-    console.log(`Instance Type: ${sku.description}`);
-    if (sku.pricingInfo && sku.pricingInfo.length > 0) {
-      const pricingExpression = sku.pricingInfo[0].pricingExpression;
-      if (pricingExpression && pricingExpression.tieredRates && pricingExpression.tieredRates.length > 0) {
-        const unitPrice = pricingExpression.tieredRates[0].unitPrice;
-        console.log(`Price: ${unitPrice.currencyCode} ${unitPrice.units}.${unitPrice.nanos / 1e6}`);
-      }
-    }
-    console.log('---');
-  });
+  try {
+    const authClient = await auth.getClient();
+    console.log('Authentication successful.');
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
-main().catch(console.error);
+authenticate();
