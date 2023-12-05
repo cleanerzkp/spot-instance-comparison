2bef391cf7ab4acc94b186679563fa82087d0e4c
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-12-05 22:03:11 +0100

    fixed price decimal

--- automation-scripts/updateGCPSpotPrices.js
+++ automation-scripts/updateGCPSpotPrices.js
@@ -11,17 +11,15 @@ const skuToInstanceRegionMap = {
     'AB94-9F50-2B3C': { instanceType: 'c2-standard-4', region: 'europe-central' }, // Warsaw (europe-central1)
     'DDBE-FFEB-7E00': { instanceType: 'c2-standard-4', region: 'near-east' }, // Israel (middleeast-north1)
     '406A-AA4B-1013': { instanceType: 'c2-standard-4', region: 'asia' }, // Mumbai (asia-south1)
-    //https://gcloud-compute.com/c2-standard-4.html to see avaialiblity of regions/skus 
+    // https://gcloud-compute.com/c2-standard-4.html to see availability of regions/skus
 
-    
     // e2-standard-4 SKUs mapping
-  'D5C5-E209-22D3': { instanceType: 'e2-standard-4', region: 'us-east' }, // Virginia (us-east4)
-  '00FD-B743-831B': { instanceType: 'e2-standard-4', region: 'us-west' }, // Los Angeles (us-west2)
-  '955B-B00E-ED15': { instanceType: 'e2-standard-4', region: 'europe-central' }, // Warsaw (europe-central1)
-  '41F4-F6BE-4AF2': { instanceType: 'e2-standard-4', region: 'near-east' }, // Israel (middleeast-north1)
-  'DFC1-04D4-B4A1': { instanceType: 'e2-standard-4', region: 'asia' } // Mumbai (asia-south1)
-    //https://cloud.google.com/skus/sku-groups/compute-engine-flexible-cud-eligible-skus  for specific sku codes
-
+    'D5C5-E209-22D3': { instanceType: 'e2-standard-4', region: 'us-east' }, // Virginia (us-east4)
+    '00FD-B743-831B': { instanceType: 'e2-standard-4', region: 'us-west' }, // Los Angeles (us-west2)
+    '955B-B00E-ED15': { instanceType: 'e2-standard-4', region: 'europe-central' }, // Warsaw (europe-central1)
+    '41F4-F6BE-4AF2': { instanceType: 'e2-standard-4', region: 'near-east' }, // Israel (middleeast-north1)
+    'DFC1-04D4-B4A1': { instanceType: 'e2-standard-4', region: 'asia' } // Mumbai (asia-south1)
+    // https://cloud.google.com/skus/sku-groups/compute-engine-flexible-cud-eligible-skus for specific SKU codes
 };
 
 async function authenticate() {
@@ -57,10 +55,12 @@ async function fetchGCPSpotPrices(authClient) {
                     const mappedData = skuToInstanceRegionMap[item.skuId];
                     const pricingInfo = item.pricingInfo[0];
                     const unitPrice = pricingInfo.pricingExpression.tieredRates[0].unitPrice;
+                    const price = parseFloat(`${unitPrice.units}.${unitPrice.nanos}`);
+                    const formattedPrice = (price * 0.1).toFixed(4); // Display price with four decimal places
                     return {
                         instanceType: mappedData.instanceType,
                         region: mappedData.region,
-                        price: parseFloat(`${unitPrice.units}.${unitPrice.nanos}`),
+                        price: formattedPrice,
                         timestamp: new Date() // Current time
                     };
                 });
