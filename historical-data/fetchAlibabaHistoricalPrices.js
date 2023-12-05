372ccc5cad895233f56f754531a8729b08fd1868
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-12-05 12:34:13 +0100

    updated historical fetch for ALB

--- historical-data/fetchAlibabaHistoricalPrices.js
+++ historical-data/fetchAlibabaHistoricalPrices.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ path: '../.env' });
 const { RPCClient } = require('@alicloud/pop-core');
 const db = require('../models');
-
 const { SpotPricing, InstanceType, Region } = db;
 
 // Function to format dates to 'YYYY-MM-DDTHH:mm:ss' (ISO 8601 without milliseconds or timezone offset)
@@ -13,15 +12,15 @@ async function fetchData() {
 
   return {
     instanceTypes: instanceTypes.map(it => ({ name: it.name, category: it.category, grouping: it.grouping })),
-    regions: regions.map(r => r.name)
+    regions: regions.map(r => ({ name: r.name, standardizedRegion: r.standardizedRegion }))
   };
 }
 
 async function fetchAlibabaSpotPrices(instanceType, region) {
-    const regionData = await Region.findOne({ where: { providerID: 'ALB', name: region } });
+    const regionData = await Region.findOne({ where: { providerID: 'ALB', name: region.name } });
   
     if (!regionData) {
-      console.error(`No region data found for ${region}`);
+      console.error(`No region data found for ${region.name}`);
       return [];
     }
   
@@ -32,106 +31,60 @@ async function fetchAlibabaSpotPrices(instanceType, region) {
         accessKeySecret: process.env.ALIBABA_ACCESS_KEY_SECRET,
         endpoint: endpoint,
         apiVersion: '2014-05-26',
-        opts: {
-          timeout: 10000  //  10 seconds timeout
-        }
-      });
-      const oneWeekAgo = new Date();
-      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-  
+        opts: { timeout: 10000 }
+    });
+
+    const startTime = new Date();
+    startTime.setDate(startTime.getDate() - 30);  // Fetch data for the past 30 days
+
     try {
       const params = {
         RegionId: regionData.name,
         NetworkType: 'vpc',
         InstanceType: instanceType.name,
-        StartTime: formatDate(oneWeekAgo),
+        StartTime: formatDate(startTime),
         EndTime: formatDate(new Date()),
         MaxResults: 500,
       };
       const result = await client.request('DescribeSpotPriceHistory', params);
-      console.log('API Response:', JSON.stringify(result, null, 2));
       return result.SpotPrices.SpotPriceType || [];
     } catch (error) {
-      console.error(`Error fetching Alibaba Spot Prices for ${region} ${instanceType.name}:`, error.message);
-      console.error('Error Details:', error);
+      console.error(`Error fetching Alibaba Spot Prices for ${region.name} ${instanceType.name}:`, error.message);
       return [];
     }
 }
 
-async function insertIntoDB(dailyAverages, instanceTypeObj, region) {
-    for (const date in dailyAverages) {
-        // Standardize the date here before any operation
-        const standardizedDate = new Date(date);
-        standardizedDate.setHours(9, 0, 0, 0);  // Set time to 9 AM
-
-        const existingRecord = await SpotPricing.findOne({
-            where: {
-                name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
-                date: standardizedDate,
-                regionCategory: `ALB-${region}`
-            }
-        });
-
-        const price = dailyAverages[date];
-
-        if (existingRecord) {
-            await existingRecord.update({ price: price, date: standardizedDate });
-        } else {
-            await SpotPricing.create({
-                name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
-                regionCategory: `ALB-${region}`,
-                date: standardizedDate, 
-                price: price,
-                timestamp: formatDate(new Date()), 
-                grouping: instanceTypeObj.grouping,
-                providerID: 'ALB'
-            });
-        }
-    }
+async function insertIntoDB(spotPriceHistory, instanceTypeObj, region) {
+  for (const spotPrice of spotPriceHistory) {
+    await SpotPricing.create({
+      name: instanceTypeObj.name,
+      regionName: region.standardizedRegion,
+      date: new Date(spotPrice.Timestamp),
+      price: parseFloat(spotPrice.SpotPrice),
+      timestamp: new Date(),
+      grouping: instanceTypeObj.grouping,
+      providerID: 'ALB'
+    });
+  }
 }
 
-  async function calculateDailyAverage(instanceTypeObj, region) {
-    try {
-      const spotPriceHistory = await fetchAlibabaSpotPrices(instanceTypeObj, region);
-  
-      if (spotPriceHistory.length === 0) {
-        console.log(`No prices available for ${instanceTypeObj.name} in ${region}`);
-        return;
-      }
-  
-      const pricesByDay = {};
-      const dailyAverages = {};
-  
-      spotPriceHistory.forEach(spotPrice => {
-        const date = new Date(spotPrice.Timestamp);
-        date.setUTCHours(9, 0, 0, 0);  // Standardize to 9 AM UTC
-        const standardizedDate = date.toISOString().split('T')[0];
-        if (!pricesByDay[standardizedDate]) {
-          pricesByDay[standardizedDate] = [];
-        }
-        pricesByDay[standardizedDate].push(parseFloat(spotPrice.SpotPrice));
-      });
-  
-      for (const date in pricesByDay) {
-        const prices = pricesByDay[date];
-        const sum = prices.reduce((acc, price) => acc + price, 0);
-        const average = sum / prices.length;
-        dailyAverages[date] = average;
-      }
-  
-      await insertIntoDB(dailyAverages, instanceTypeObj, region);
-    } catch (error) {
-      console.error('Error calculating daily average:', error.message);
-    }
+async function processSpotPrices(instanceTypeObj, region) {
+  const spotPriceHistory = await fetchAlibabaSpotPrices(instanceTypeObj, region);
+  if (spotPriceHistory.length === 0) {
+    console.log(`No prices available for ${instanceTypeObj.name} in ${region.name}`);
+    return;
   }
+
+  await insertIntoDB(spotPriceHistory, instanceTypeObj, region);
+}
 
 async function main() {
   const { instanceTypes, regions } = await fetchData();
-  instanceTypes.forEach(instanceTypeObj => {
-    regions.forEach(region => {
-      calculateDailyAverage(instanceTypeObj, region);
-    });
-  });
+  for (const instanceTypeObj of instanceTypes) {
+    for (const region of regions) {
+      await processSpotPrices(instanceTypeObj, region);
+    }
+  }
 }
 
 main();
