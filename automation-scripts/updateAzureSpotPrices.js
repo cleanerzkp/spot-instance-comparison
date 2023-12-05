35707794b477558b873725ea3a7b41c16c1febaf
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-12-05 22:46:39 +0100

    new schema updated for azure

--- automation-scripts/updateAzureSpotPrices.js
+++ automation-scripts/updateAzureSpotPrices.js
@@ -26,55 +26,31 @@ async function fetchAzurePrices(instanceType, region) {
 
   try {
     const response = await axios.get(apiUrl, { params });
-    if (response.data && response.data.Items) {
-      return response.data.Items;
-    } else {
-      console.log(`No price data found for ${instanceType} in ${region}`);
-      return [];
-    }
+    return response.data && response.data.Items ? response.data.Items : [];
   } catch (error) {
     console.error(`Error fetching prices for ${instanceType} in ${region}:`, error.message);
     return [];
   }
 }
-async function insertIntoDB(avgPrices, instanceTypeObj, region) {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
 
-  try {
-    for (const [region, avgPrice] of Object.entries(avgPrices)) {
-      const existingRecord = await db.SpotPricing.findOne({
-        where: {
-          name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
-          regionCategory: `AZR-${region}`,
-          date: today
-        }
-      });
+async function insertIntoDB(prices, instanceTypeObj, region) {
+  const currentTime = new Date();
 
-      if (existingRecord) {
-        await existingRecord.update({
-          price: avgPrice,
-          timestamp: today,
-          updatedAt: today
-        });
-      } else {
-        await db.SpotPricing.create({
-          name: `${instanceTypeObj.name}-${instanceTypeObj.category}`,
-          regionCategory: `AZR-${region}`,
-          date: today,
-          price: avgPrice,
-          timestamp: today,
-          createdAt: today,
-          updatedAt: today,
-          grouping: instanceTypeObj.grouping,
-          providerID: 'AZR'
-        });
-      }
-    }
-  } catch (error) {
-    console.error('Error inserting data into database:', error.message);
+  for (const price of prices) {
+    await db.SpotPricing.create({
+      name: instanceTypeObj.name,
+      regionName: region,
+      price: parseFloat(price.retailPrice),
+      date: currentTime,
+      timestamp: currentTime,
+      createdAt: currentTime,
+      updatedAt: currentTime,
+      grouping: instanceTypeObj.grouping,
+      providerID: 'AZR'
+    });
   }
 }
+
 async function main() {
   const { instanceTypes, regions } = await fetchData();
   if (instanceTypes.length === 0 || regions.length === 0) {
@@ -89,21 +65,7 @@ async function main() {
       promises.push((async () => {
         const prices = await fetchAzurePrices(instanceTypeObj.name, region);
         if (prices.length > 0) {
-          const avgPrices = prices.reduce((acc, price) => {
-            const priceValue = parseFloat(price.retailPrice);
-            if (!isNaN(priceValue) && priceValue <= 10) { 
-              acc[region] = (acc[region] || [0, 0]);
-              acc[region][0] += priceValue;
-              acc[region][1] += 1;
-            }
-            return acc;
-          }, {});
-
-          for (const region in avgPrices) {
-            avgPrices[region] = avgPrices[region][0] / avgPrices[region][1];
-          }
-
-          await insertIntoDB(avgPrices, instanceTypeObj, region);
+          await insertIntoDB(prices, instanceTypeObj, region);
         } else {
           console.log(`No prices available for ${instanceTypeObj.name} in ${region}`);
         }
@@ -114,8 +76,4 @@ async function main() {
   await Promise.all(promises);
 }
 
-main().catch(error => console.error('Error in main function:', error.message));async function runAzureScript() {
-  await main();
-}
-
-module.exports = runAzureScript;
+main().catch(error => console.error('Error in main function:', error.message));
